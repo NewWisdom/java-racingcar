b4f30aa1cbcbcc489456520390c07da415ca5614
NewWisdom <wlgp2500@gmail.com>
2021-02-15 11:35:50 +0900

    refactor(cars): 정적 팩토리 메서드 추가에 따른 방어적 복사 테스트 삭제, 최대 position car 확인 테스트 수정

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -3,9 +3,7 @@ package racingcar.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
@@ -15,34 +13,20 @@ public class CarsTest {
     @DisplayName("자동차들 이름이 중복되었을 경우")
     void carsDuplicatedName() {
         assertThatThrownBy(() -> {
-            Cars cars = new Cars(
-                    Arrays.asList(
-                            new Car(new Name("pobi")),
-                            new Car(new Name("pobi")),
-                            new Car(new Name("pobi"))
-                    ));
+            Cars cars = Cars.makeFromCarNames(Arrays.asList(
+                    new Name("pobi"),
+                    new Name("pobi"),
+                    new Name("pobi")));
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("자동차들 중 최대 position car 확인")
     void carsGetMaxPosition() {
-        Cars cars = new Cars(
-                Arrays.asList(
-                        new Car(new Name("pobi"), 0),
-                        new Car(new Name("amazi"), 3)
-                ));
-        assertThat(cars.getMaxPositionCar())
-                .isEqualTo(new Car(new Name("amazi"), 3));
-    }
-
-    @Test
-    void defensiveCopy() {
-        final List<Car> cars = new ArrayList<>();
-        final Cars actual = new Cars(cars);
-
-        cars.add(new Car(new Name("amazi")));
-
-        assertThat(actual.cars()).isEmpty();
+        Cars cars = Cars.makeFromCarNames(Arrays.asList(
+                new Name("pobi"),
+                new Name("amazi")));
+        assertThat(cars.getMaxPositionCar()
+                .getPosition()).isEqualTo(0);
     }
 }
